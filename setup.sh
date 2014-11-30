9296f42f94537037d193bf00e06e92e49ff75141
Dawid Romanowski <dawidromanowski13@gmail.com>
2014-11-30 13:30:04 +0100

    Costam jeszcze porobilem

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 curl -X PUT http://anna:ala123@127.0.0.1:5984/tai-couch
-curl -H "Content-type: application/json" -d @posts.json -X POST http://anna:ala123@127.0.0.1:5984/tai-couch/_bulk_docs
+#curl -H "Content-type: application/json" -d @posts.json -X POST http://anna:ala123@127.0.0.1:5984/tai-couch/_bulk_docs
 couchapp push . http://anna:ala123@127.0.0.1:5984/tai-couch
